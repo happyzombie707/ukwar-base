9cbcd6cc6e70dadab71eace03bab8da151403fb3
Orlando <landocrawford@gmail.com>
2017-04-27 19:15:29 +0100

    Update core

---
+++ gamemode/gravitygun.lua
@@ -0,0 +1,37 @@
+RUNTIME_LOG("ENTERED gravity_gun.lua")
+
+--[[---------------------------------------------------------
+   Name: gamemode:GravGunPunt()
+   Desc: We're about to punt an entity (primary fire).
+		 Return true if we're allowed to.
+-----------------------------------------------------------]]
+function GM:GravGunPunt( ply, ent )
+	return false
+end
+
+--[[---------------------------------------------------------
+	Name: gamemode:GravGunPickupAllowed()
+	Desc: Return true if we're allowed to pickup entity
+-----------------------------------------------------------]]
+function GM:GravGunPickupAllowed( ply, ent )
+	return true
+end
+
+if ( SERVER ) then
+
+	--[[---------------------------------------------------------
+	   Name: gamemode:GravGunOnPickedUp()
+	   Desc: The entity has been picked up
+	-----------------------------------------------------------]]
+	function GM:GravGunOnPickedUp( ply, ent )
+	end
+
+
+	--[[---------------------------------------------------------
+	   Name: gamemode:GravGunOnDropped()
+	   Desc: The entity has been dropped
+	-----------------------------------------------------------]]
+	function GM:GravGunOnDropped( ply, ent )
+	end
+
+end
